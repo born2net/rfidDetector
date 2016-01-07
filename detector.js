6bdfd3ab9b91761f94f0dbe7b3749c5ba6c60daf
root <born2net@gmail.com>
2016-01-06 18:18:26 -0800

    milestone

--- detector.js
+++ detector.js
@@ -29,11 +29,14 @@ for (var i in tags) {
 }
 
 var source1 = Rx.Observable.create(function (observer) {
+
     child.stdout.on('data', function (stdout) {
         var re = 'rssi\ (.*)(, epc)(.*), count\ ([0-9A-Za-z]+)';
         var match = stdout.match(new RegExp(re));
+
         if (match == null)
             return;
+
         var data = {
             distance: match[1] * -1,
             tag: match[3],
@@ -41,14 +44,18 @@ var source1 = Rx.Observable.create(function (observer) {
         };
         observer.next(data);
     });
+
     child.stderr.on('data', function (data) {
-        console.log('stdout: ' + data);
-    });
-    child.on('close', function (code) {
-        console.log('closing code: ' + code);
+        //console.log('stdout: ' + data);
     });
 
-}).throttleTime(5000).map(data=> {
+    child.on('close', function (code) {
+        //console.log('closing code: ' + code);
+    });
+
+
+}).throttleTime(250).map(data=> {
+
     // ignore unknown tags
     var tag = data.tag.trim();
     if (!database[tag]) {
@@ -56,31 +63,28 @@ var source1 = Rx.Observable.create(function (observer) {
         return null;
     }
 
+    // fill with latest values off the wire
     database[tag].oldDistance = database[tag].newDistance;
     database[tag].newDistance = data.distance;
     database[tag].totalSeen = database[tag].totalSeen + 1;
-
     return database[tag];
 });
 
-var source22 = Rx.Observable.interval(100).map(e=> {
+var source2 = Rx.Observable.interval(1000).map(e=> {
     return Rx.Observable.create(function (observer) {
         for (var tag in database) {
             observer.next(database[tag]);
         }
     });
+}).flatMap(e=> {
+    return e
 });
 
-var source2 = source22.flatMap(e=> {
-    return e
-})
 
-
-var source3 = Rx.Observable.merge(source1, source2);
-
-var subscription = source3.subscribe(
+var final = Rx.Observable.merge(source1, source2).distinctUntilChanged();
+final.subscribe(
     function (x) {
-        console.log(x);
+        console.log(`'data ${JSON.stringify(x)}`);
     },
     function (err) {
         console.log('Error: ' + err);
@@ -89,60 +93,3 @@ var subscription = source3.subscribe(
         console.log('Completed');
     });
 
-
-/*
- outputLiveTags.throttleTime(5).map(data=> {
- console.log('AAAAAAAAAAAAAAAAAAAAAAAAAAA')
- // ignore unknown tags
- var tag = data.tag.trim();
- //if (!database[tag]) {
- //    //console.log('ignoring unknown tag ' + tag);
- //    return null;
- //}
-
- database[tag].oldDistance = database[tag].newDistance;
- database[tag].newDistance = data.distance;
- database[tag].totalSeen = database[tag].totalSeen + 1;
-
- return database[tag];
-
- })
-
- var final = Rx.Observable.merge(outputAllTags, outputLiveTags);
-
- final.subscribe(e=> {
- console.log(`Final ${JSON.stringify(e)}`);
-
- //var newDistance = Math.abs(data.distance)
- //var oldDistance = Math.abs(database[tag].distance)
- //var differenceInDistance = Math.abs(newDistance - oldDistance);
- //if (oldDistance != 0 &&
- //    differenceInDistance > minDistanceMoved) {
- //    console.log(`Sample id moved ' ${tag} old: ${oldDistance} new: ${newDistance}`);
- //}
-
- });
-
- child.stdout.on('data', function (data) {
- var re = 'rssi\ (.*)(, epc)(.*), count\ ([0-9A-Za-z]+)';
- var match = data.match(new RegExp(re));
- if (match == null)
- return;
- var data = {
- distance: match[1] * -1,
- tag: match[3],
- count: match[4]
- };
- if (!inputLiveTags)
- return;
- inputLiveTags.next(data);
-
- //console.log('stdout: ' + data);
- });
- child.stderr.on('data', function (data) {
- console.log('stdout: ' + data);
- });
- child.on('close', function (code) {
- console.log('closing code: ' + code);
- });
- */
